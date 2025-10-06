e244c9c76f38abc772ab0fe9374dfe376104df9f
abdeljamil <113044449+abdeljamil@users.noreply.github.com>
2025-10-06 12:16:11 +0100

    update

---
+++ affichage_simple.py
@@ -0,0 +1,12 @@
+"""
+EXERCICE PYTHON #1
+>Afficher le texte suivant en sortie dans la console (ligne de commande)
+,en respectant les retours à la ligne, espace et tabulation
+
+
+LANGAGE PYTHON
+    >Domaine :programmetion
+    >Créateur :Guido van Rossum
+    >Site     :https://www.python.org
+
+"""
