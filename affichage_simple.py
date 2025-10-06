97a457ad045c0a6bb0e662055dd9aadb9759abbd
abdeljamil <113044449+abdeljamil@users.noreply.github.com>
2025-10-06 17:41:59 +0100

    update a file

--- affichage_simple.py
+++ affichage_simple.py
@@ -5,8 +5,32 @@ EXERCICE PYTHON #1
 
 
 LANGAGE PYTHON
-    >Domaine :programmetion
-    >Créateur :Guido van Rossum
-    >Site     :https://www.python.org
+    > Domaine :programmetion
+    > Créateur :Guido van Rossum
+    > ite     :https://www.python.org
 
 """
+
+print("Ceci est une texte")
+
+print("Ceci est un\ 
+autre texte")
+
+print("LANGAGE PYTHON")
+print("\t> Domain   : programmation")
+print("\t> Créateur : Guido van Rossum")
+print("\t> Site     : https://www.python.org")
+
+
+
+#Ou alors.......
+
+
+print("LAGARGE PYTHON\n\ 
+\t> Domain    : programmation\n\
+\t> Créateur  : Guido van Rossum\n\
+\t> Site      :  https://www.python.org")
+
+
+
+
