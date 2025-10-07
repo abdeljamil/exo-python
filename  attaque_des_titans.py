ef6ae40e05e281c182ee704f8e6e949037dc6be2
abdeljamil <113044449+abdeljamil@users.noreply.github.com>
2025-10-07 08:33:52 +0100

    update a file

---
+++  attaque_des_titans.py
@@ -0,0 +1,17 @@
+"""
+EXERCICE PYTHON #3
+
+> Créer un programme simulant  un combat qui respecte les contraintes suivantes:
+    - Deux joueurs, aux quels on demandera de choisir un pseudo
+    - Les deux combattants démarrent avec 250 point de vie chacun 
+    - Le combat se déroule en 4 attaques (Joueur1,joueur2,joueur1 et enfin joueur2)
+    - Chaque attaque est une tentative (-Si elle réussit,le joueur infligera un nombre de dégats entre 0 et 100
+                                        -Si elle échoue,l'attaque est ratée, et c'est au tour de l'autre joueur)
+    - A la fin du combat (les 4 attaques), on déclare le gagnant (celui à qui il reste le plus de point de vie)
+> Indications :
+    - Le déroulement du combat doit être logique et annoncé à l'utilisateur (affichez du texte, décrivez ce qu'il se passe)
+    - Coder dans un premier temps uniquement avec des afficharges/saisies,variables,opérations,conditions.
+    - Pour les plus avancés, vous pourrez optimiser ce code ensuite en l'adaptant avec vos connaissance (boucles,fonction,classe,etc..)
+
+"""
+
